c4fdc92742cfc8bc5384e06ebad5b735a6281ebc
cdl08001 <calin.lewis@gmail.com>
2018-07-10 14:55:03 -0700

    Implement serving static files

--- challenge_2/server/server.js
+++ challenge_2/server/server.js
@@ -1,31 +1,44 @@
-var express = require('express');
-var app = express();
-var convertData = require('./formatData')
+const express = require('express');
 
-app.use(function(req, res, next) {
-  console.log('A request has been recieved!')
+const app = express();
+
+const path = require('path');
+
+const clientDirectory = path.dirname(__dirname) + '/client';
+
+const convertData = require('./formatData');
+
+
+app.use(express.static(clientDirectory));
+
+app.get('/', (req, res) => {
+  res.send('This is the GET response');
+});
+
+app.use((req, res, next) => {
+  console.log('A request has been recieved!');
   next();
-})
+});
 
 app.post('/', (req, res, next) => {
   // We will use this middleware to confirm receipt of the data
-  var receivedData;
+  let receivedData;
 
-  req.on('data', function(data){
-    console.log('The server is processing the request...')
-    var receivedData = JSON.parse(data);
-    var convertedData = convertData(receivedData);
+  req.on('data', (data) => {
+    console.log('The server is processing the request...');
+    receivedData = JSON.parse(data);
+    const convertedData = convertData(receivedData);
     console.log('The data has veen processed: ', convertedData);
   })
-  .on('end', function(){
-    res.send('This is the POST response')
-    next();
-  })
-})
+    .on('end', () => {
+      res.send('This is the POST response');
+      next();
+    });
+});
 
-app.listen(3000, () => console.log('The server is listening on port 3000!')); 
+app.listen(3000, () => console.log('The server is listening on port 3000!'));
 
 /*
 TODO:
- - Check the data. If incoming data is undefined, we need to send back and error. 
+ - Check the data. If incoming data is undefined, we need to send back and error.
 */
