4546677a2d709b3514030584e5ae609a092ec21e
cdl08001 <calin.lewis@gmail.com>
2018-07-10 11:30:37 -0700

    Implement server middleware chaining

--- challenge_2/server/server.js
+++ challenge_2/server/server.js
@@ -3,8 +3,30 @@ var app = express();
 
 app.use(function(req, res, next) {
   console.log('A request has been recieved!')
-  console.log('Request: ', req);
-  console.log('Response: ', res);
+  next();
+})
+
+app.post('/', (req, res, next) => {
+  // We will use this middleware to recieve the data
+  req.on('data', function(data){
+    var recievedData = JSON.parse(data);
+    console.log(recievedData);
+  })
+  req.on('end', function(){
+    console.log('We have recieved the data and need to format it')
+    next();
+  })
+})
+
+app.post('/', (req, res, next) => {
+  // We will use this middleware to convert the data.
+  console.log('We are working on converting the data');
+  next()
+})
+
+app.post('/', (req, res) => {
+  // We will use this middleware to send the data.
+  res.send('This is the POST response');
 })
 
 
